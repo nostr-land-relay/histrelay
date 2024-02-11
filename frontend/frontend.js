55e05c584731a716a6ae308132eaa133802718ef
Semisol <hi@semisol.dev>
2024-02-11 04:28:00 +0300

    initial commit

---
+++ frontend/frontend.js
@@ -0,0 +1,171 @@
+let signInButton = document.getElementById("sign-in")
+let statusBox = document.getElementById("statusbox")
+let defaultRelays = [
+    "wss://hist.nostr.land",
+    "wss://nostr.land",
+    "wss://relay.damus.io",
+    "wss://nos.lol",
+    "wss://nostr.mutinywallet.com"
+]
+
+signInButton.addEventListener("click", async () => {
+    if (!window.nostr) {
+        signInButton.disabled = true
+        signInButton.innerText = "no extension detected"
+        return
+    }
+    let pk
+    try {
+        pk = await nostr.getPublicKey()
+    } catch (e) {
+        return alert("error: " + e)
+    }
+    loggedIn(pk)
+})
+
+async function loggedIn(pk) {
+    signInButton.disabled = true
+    statusBox.innerText = "connecting to relay"
+    let relay = await NostrTools.Relay.connect("ws://localhost:4834")
+    statusBox.innerText = "getting events"
+    let events = []
+    const sub = relay.subscribe([
+        {
+            authors: [pk],
+            kinds: [0, 3]
+        },
+    ], {
+        onevent(event) {
+            events.push(event)
+        },
+        oneose() {
+            statusBox.innerText = "done"
+            showList(events)
+            sub.close()
+        }
+    })
+}
+
+function showList(list) {
+    let kinds = [[0, "profiles"], [3, "contact lists"]]
+    ;[...statusBox.childNodes].forEach(el => el.remove())
+    kinds.forEach(([kind, titlestr]) => {
+        let title = document.createElement("h3")
+        title.innerText = titlestr + " (kind " + kind + ")"
+        statusBox.appendChild(title)
+        let kindEvents = list.filter(el => el.kind === kind)
+        if (kindEvents.length === 0) {
+            let p = document.createElement("p")
+            p.innerText = "none found - add wss://hist.nostr.land"
+            statusBox.appendChild(p)
+        } else {
+            kindEvents = kindEvents.slice(0, 10)
+            kindEvents = kindEvents.sort((a, b) => b.created_at - a.created_at)
+            let table = document.createElement("table")
+            let entries = [
+                {row: ["id", "description", "date", "restore"], evt: null},
+                ...kindEvents.map(el => {
+                    let desc = "unknown"
+                    if (el.kind === 0) {
+                        let data
+                        try {
+                            data = JSON.parse(el.content)
+                        } catch (e) {
+                            desc = "corrupted event"
+                            console.error(e)
+                        }
+                        if (data) {
+                            desc = `name: ${data?.name || "none"}, nip05: ${data?.nip05 || "none"}`
+                        }
+                    }
+                    let d = new Date(el.created_at * 1000)
+                    return {
+                        row: [el.id.slice(0, 8) + "...", desc, d.toLocaleDateString() + " " + d.toLocaleTimeString(), Symbol.for("histrelay:restore")],
+                        evt: el
+                    }
+                })
+            ]
+            entries.forEach((el, i) => {
+                let tr = document.createElement("tr")
+                el.row.forEach(el2 => {
+                    let td = document.createElement(i === 0 ? "th" : "td")
+                    if (el2 === Symbol.for("histrelay:restore")) {
+                        let button = document.createElement("button")
+                        button.innerText = "restore"
+                        button.addEventListener("click", () => {
+                            restore(el.evt)
+                        })
+                        td.appendChild(button)
+                    } else {
+                        td.innerText = el2
+                    }
+                    tr.appendChild(td)
+                })
+                table.appendChild(tr)
+            })
+            statusBox.appendChild(table)
+        }
+    })
+}
+
+async function restore(evt) {
+    evt = structuredClone(evt)
+    evt.created_at = Math.floor(Date.now() / 1000)
+    evt.id = NostrTools.getEventHash(evt)
+    delete evt.sig
+    let relaysObj
+    try {
+        relaysObj = await nostr.getRelays()
+    } catch (e) {
+        alert("error getting relays: " + e)
+        return
+    }
+    let signed
+    try {
+        signed = await nostr.signEvent(evt)
+    } catch (e) {
+        alert("error signing: " + e)
+        return
+    }
+    let relays = [...new Set([...defaultRelays, ...Object.entries(relaysObj)])]
+    ;[...statusBox.childNodes].forEach(el => el.remove())
+    let title = document.createElement("h3")
+    title.innerText = "broadcast status"
+    let table = document.createElement("table")
+    let relayStatuses = {}
+    ;["relay", ...relays].forEach((relay, i) => {
+        let tr = document.createElement("tr")
+        let td1 = document.createElement(i === 0 ? "th" : "td")
+        let td2 = document.createElement(i === 0 ? "th" : "td")
+        td1.innerText = relay
+        td2.innerText = i === 0?"status":"connecting..."
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        if (i !== 0) {
+            relayStatuses[relay] = td2
+        }
+        table.appendChild(tr)
+    })
+    statusBox.appendChild(table)
+    relays.forEach(async el => {
+        let relay
+        try {
+            relay = await NostrTools.Relay.connect(el)
+        } catch(e) {
+            console.error(e)
+            relayStatuses[el].innerText = "error connecting"
+            return
+        }
+        relayStatuses[el].innerText = "sending event"
+        let interval = setTimeout(() => {
+            relayStatuses[el].innerText = "timed out"
+        }, 5000)
+        try {
+            await relay.publish(signed)
+            relayStatuses[el].innerText = "done"
+        } catch(e) {
+            relayStatuses[el].innerText = "rejected: " + e
+        }
+        clearInterval(interval)
+    })
+}
---
+++ main.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/fiatjaf/khatru"
+	"github.com/mailru/easyjson"
+	"github.com/nbd-wtf/go-nostr"
+	"golang.org/x/exp/slices"
+)
+
+var acceptableEventKinds = []int{0, 3}
+var keepAtMost = 10
+
+func main() {
+	var err error
+
+	db, err := sql.Open("sqlite3", "file:./data.db?_busy_timeout=5000&_mutex=full")
+	assert(err)
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS events (
+		id BLOB NOT NULL PRIMARY KEY,
+		pubkey BLOB NOT NULL,
+		kind TEXT NOT NULL,
+		created_at INTEGER NOT NULL,
+		event TEXT NOT NULL
+	);
+	CREATE INDEX IF NOT EXISTS events__created_at ON events(created_at);
+	CREATE INDEX IF NOT EXISTS events__kind__created_at ON events(kind, created_at);
+	CREATE INDEX IF NOT EXISTS events__pubkey__created_at ON events(pubkey, created_at);
+	CREATE INDEX IF NOT EXISTS events__pubkey__kind__created_at ON events(pubkey, kind, created_at);
+	`)
+	assert(err)
+
+	relay := khatru.NewRelay()
+	relay.Info.Name = "history relay"
+	relay.Info.Description = "keeps history of your profile and contact list"
+	relay.StoreEvent = append(relay.StoreEvent,
+		func(ctx context.Context, event *nostr.Event) error {
+			bId, err := hex.DecodeString(event.PubKey)
+			if err != nil {
+				return err
+			}
+			q := db.QueryRowContext(ctx, "SELECT 1 FROM events WHERE id = ?", bId)
+			if q.Err() != nil {
+				return q.Err()
+			}
+			var bogus int
+			err = q.Scan(&bogus)
+			if sql.ErrNoRows == err {
+				bPk, err := hex.DecodeString(event.PubKey)
+				if err != nil {
+					return err
+				}
+				q = db.QueryRowContext(ctx, "SELECT id, created_at FROM events WHERE pubkey = ? AND kind = ? ORDER BY created_at DESC, id ASC LIMIT 1 OFFSET ?", bPk, event.Kind, keepAtMost-1)
+				if q.Err() != nil {
+					return q.Err()
+				}
+				var (
+					lId        []byte
+					created_at int64
+				)
+				err = q.Scan(&lId, &created_at)
+				if err != nil {
+					if err != sql.ErrNoRows {
+						return err
+					}
+				} else {
+					if created_at > int64(event.CreatedAt) || (created_at == int64(event.CreatedAt) &&
+						bytes.Compare(bId, lId) == 1) {
+						return fmt.Errorf("blocked: this event is too old")
+					}
+				}
+				_, err = db.ExecContext(ctx, "INSERT INTO events VALUES (?, ?, ?, ?, ?)", bId, bPk, event.Kind, event.CreatedAt, event.String())
+				if err != nil {
+					return err
+				}
+				_, err = db.ExecContext(ctx, "DELETE FROM events WHERE id IN (SELECT id FROM events WHERE pubkey = ? AND kind = ? ORDER BY created_at DESC, id ASC LIMIT -1 OFFSET ?)", bPk, event.Kind, keepAtMost-1)
+				if err != nil {
+					return err
+				}
+				return nil
+			} else if err != nil {
+				return err
+			} else {
+				return fmt.Errorf("duplicate")
+			}
+		},
+	)
+	relay.QueryEvents = append(relay.QueryEvents,
+		func(ctx context.Context, filter nostr.Filter) (chan *nostr.Event, error) {
+			ch := make(chan *nostr.Event)
+			var query = make([]string, 0, 3)
+			var queryParams = make([]any, 0, 5)
+			if filter.Since != nil {
+				queryParams = append(queryParams, int64(*filter.Since))
+				query = append(query, "created_at > ?")
+			}
+			if filter.Until != nil {
+				queryParams = append(queryParams, int64(*filter.Until))
+				query = append(query, "created_at < ?")
+			}
+			if len(filter.Kinds) > 0 {
+				var ctr = 0
+				for _, k := range filter.Kinds {
+					if !slices.Contains(acceptableEventKinds, k) {
+						continue
+					}
+					queryParams = append(queryParams, k)
+					ctr++
+				}
+				query = append(query, "kind IN ("+strings.Repeat("?,", ctr-1)+"?)")
+			}
+			if len(filter.Authors) > 0 {
+				for _, a := range filter.Authors {
+					pk, err := hex.DecodeString(a)
+					if err != nil {
+						return nil, err
+					}
+					queryParams = append(queryParams, pk)
+				}
+				query = append(query, "pubkey IN ("+strings.Repeat("?,", len(filter.Authors)-1)+"?)")
+			}
+			if len(filter.IDs) > 0 {
+				for _, a := range filter.IDs {
+					id, err := hex.DecodeString(a)
+					if err != nil {
+						return nil, err
+					}
+					queryParams = append(queryParams, id)
+				}
+				query = append(query, "id IN ("+strings.Repeat("?,", len(filter.IDs)-1)+"?)")
+			}
+			if len(query) == 0 {
+				query = append(query, "1")
+			}
+			go func() {
+				defer close(ch)
+				q := db.QueryRowContext(ctx, "SELECT event FROM events WHERE "+strings.Join(query, " AND "), queryParams...)
+				if q.Err() != nil {
+					fmt.Println(q.Err())
+					return
+				}
+				var ctr = 0
+				var scanctr = 0
+				for {
+					if scanctr >= 2000 {
+						break
+					}
+					if (filter.Limit != 0 && ctr >= filter.Limit) || ctr >= 500 {
+						break
+					}
+					var evt string
+					err = q.Scan(&evt)
+					if sql.ErrNoRows == err {
+						break
+					} else if err != nil {
+						fmt.Println(err)
+						break
+					}
+					scanctr++
+					nevt := nostr.Event{}
+					easyjson.Unmarshal([]byte(evt), &nevt)
+					if !filter.Matches(&nevt) {
+						continue
+					}
+					ch <- &nevt
+				}
+			}()
+			return ch, nil
+		},
+	)
+
+	relay.RejectEvent = append(relay.RejectEvent,
+		func(ctx context.Context, event *nostr.Event) (reject bool, msg string) {
+			if !slices.Contains(acceptableEventKinds, event.Kind) {
+				return true, "blocked: this relay only accepts kind 0/3"
+			}
+			bId, err := hex.DecodeString(event.PubKey)
+			if err != nil {
+				return true, "error: " + err.Error()
+			}
+			q := db.QueryRowContext(ctx, "SELECT 1 FROM events WHERE id = ?", bId)
+			if q.Err() != nil {
+				return true, "error: " + err.Error()
+			}
+			var bogus int
+			err = q.Scan(&bogus)
+			if err == nil {
+				return true, "duplicate:"
+			} else if sql.ErrNoRows != err {
+				return true, "error: " + err.Error()
+			}
+			return false, ""
+		},
+	)
+
+	fmt.Println("running on :4834")
+	http.ListenAndServe(":4834", relay)
+}
+
+func assert(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
